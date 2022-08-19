a927df606aa8105fe3c42fb4da34009aafb1f375
Francisco Anzueto <franzueto@gmail.com>
2022-08-18 19:22:03 -0600

    Initial state.

--- app/src/main/java/com/franzueto/lab1/MainActivity.kt
+++ app/src/main/java/com/franzueto/lab1/MainActivity.kt
@@ -5,49 +5,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    companion object {
-        const val MESSAGE_KEY = "MESSAGE_KEY"
-    }
-
-    var message: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        savedInstanceState?.let {
-            message = it.getString(MESSAGE_KEY)
-        }
-
-        message?.let {
-            sayHi(it)
-        }
 
         val button = findViewById<Button>(R.id.button)
 
         button.setOnClickListener {
             val editText = findViewById<EditText>(R.id.editTextTextPersonName)
             val message = editText.text.toString()
-            sayHi(message)
-            this.message = message
-//            val intent = Intent(this, DisplayMessageActivity::class.java).apply {
-//                putExtra(DisplayMessageActivity.MESSAGE_KEY, message)
-//            }
-//            startActivity(intent)
+            val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+                putExtra(DisplayMessageActivity.MESSAGE_KEY, message)
+            }
+            startActivity(intent)
         }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putString(MESSAGE_KEY, message)
-    }
-
-    private fun sayHi(message: String) {
-        val hiMessage = getString(R.string.say_hi)
-        findViewById<TextView>(R.id.textView).text = "$hiMessage $message"
     }
 }
