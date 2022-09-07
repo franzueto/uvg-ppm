f95413d77e29e5008879d01ea536b3cf840c4dc9
Francisco Anzueto <franzueto@gmail.com>
2022-09-06 19:24:58 -0600

    Add AppBar Menu - Open an External browser to search a 'pizza' text through an implicit Intent.

--- app/src/main/java/com/franzueto/lab1/MainActivity.kt
+++ app/src/main/java/com/franzueto/lab1/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.franzueto.lab1
 
+import android.app.SearchManager
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -23,5 +27,23 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.settings_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> {
+                val intent = Intent(Intent.ACTION_WEB_SEARCH)
+                intent.putExtra(SearchManager.QUERY, "pizza")
+                if (intent.resolveActivity(packageManager) != null) {
+                    startActivity(intent)
+                }
+                true
+            } else -> super.onOptionsItemSelected(item)
+        }
     }
 }
