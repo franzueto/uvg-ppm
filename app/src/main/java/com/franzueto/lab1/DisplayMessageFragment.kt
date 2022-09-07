f94f381ee36414eb7b01205be942f104a87f54dc
Francisco Anzueto <franzueto@gmail.com>
2022-09-06 19:15:17 -0600

    Enable View Binding

--- app/src/main/java/com/franzueto/lab1/DisplayMessageFragment.kt
+++ app/src/main/java/com/franzueto/lab1/DisplayMessageFragment.kt
@@ -1,21 +1,34 @@
 package com.franzueto.lab1
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.TextView
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import com.franzueto.lab1.databinding.ActivityDisplayMessageBinding
 
-class DisplayMessageFragment : Fragment(R.layout.activity_display_message) {
+class DisplayMessageFragment : Fragment() {
+
+    private var _binding: ActivityDisplayMessageBinding? = null
+    private val binding get() = _binding!!
 
     val arg: DisplayMessageFragmentArgs by navArgs()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = ActivityDisplayMessageBinding.inflate(layoutInflater, container, false)
+
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //val message = intent.getStringExtra(DisplayMessageActivity.KEY_EXTRA_TEXT)
-
-        view.findViewById<TextView>(R.id.messageTextView).apply {
+        binding.messageTextView.apply {
             text = arg.message
         }
     }
--- app/src/main/java/com/franzueto/lab1/MainFragment.kt
+++ app/src/main/java/com/franzueto/lab1/MainFragment.kt
@@ -1,22 +1,33 @@
 package com.franzueto.lab1
 
-import android.content.ActivityNotFoundException
-import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.Button
+import android.view.ViewGroup
 import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.franzueto.lab1.databinding.FragmentMainBinding
 
-class MainFragment : Fragment(R.layout.fragment_main) {
+class MainFragment : Fragment() {
+
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val button = view.findViewById<Button>(R.id.button)
-
-        button.setOnClickListener {
+        binding.button.setOnClickListener {
             val editText = view.findViewById<EditText>(R.id.editTextTextPersonName)
             val message = editText.text.toString()
 
@@ -24,5 +35,10 @@ class MainFragment : Fragment(R.layout.fragment_main) {
 
             findNavController().navigate(action)
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
